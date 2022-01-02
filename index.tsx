ddd40676ef0d883ebf127bba227f00772a117ad9
dimlok <kirasakuya@outlook.de>
2022-01-02 21:16:58 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import styled from 'styled-components';
+const Test = styled.div`
+color:red;
+font-size: 2rem;
+`;
+
+const App = () => {
+  const name = 'test';
+  return (
+    <div>
+      <Test>{name}</Test>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
